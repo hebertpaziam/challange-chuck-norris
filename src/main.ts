8dcd50ceb79c9c0aa603993aa492db85a64a69d1
Hebert Paziam <hebertpazian@hotmail.com>
2020-12-06 00:25:51 -0300

    chore(router): set page title watching route getting title from router meta

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,13 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  watch: {
+    $route: {
+      immediate: true,
+      handler(to) {
+        document.title = to.meta.title || 'Chuck Norris Facts';
+      }
+    }
+  },
   render: (h) => h(App)
 }).$mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,27 @@ const router = new VueRouter({
     {
       path: '/',
       name: 'Home',
+      meta: {
+        title: 'Chuck Norris Facts - Home'
+      },
       component: Home
+    },
+    {
+      path: '/fact-list',
+      name: 'FactList',
+      meta: {
+        title: 'Chuck Norris Facts - List'
+      },
+
+      component: () => import('../views/FactList.vue')
+    },
+    {
+      path: '/fact-details',
+      name: 'FactDetails',
+      meta: {
+        title: 'Chuck Norris Facts - Details'
+      },
+      component: () => import('../views/FactDetails.vue')
     }
   ]
 });
